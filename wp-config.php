b5c87fc0da07cedff3dabaf1e888accff0c53388
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-01-02 22:15:13 +0600

    wordpress updated

--- wp-config.php
+++ wp-config.php
@@ -79,7 +79,7 @@ $table_prefix  = 'wp_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define('WP_DEBUG', false);
+
 
 /* That's all, stop editing! Happy blogging. */
 
@@ -89,3 +89,5 @@ if ( !defined('ABSPATH') )
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
+
+define('WP_DEBUG', false);
